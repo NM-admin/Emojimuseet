4efb513dfa43d2f870f3339de2c8265c47fbaf0a
Aron Ambrosiani <aron@ambrosiani.se>
2017-10-19 23:47:46 +0200

    Facebook.bot

--- src/facebook.js
+++ src/facebook.js
@@ -8,8 +8,31 @@ let bot = new Bot({
   app_secret: process.env.FACEBOOK_APP_SECRET
 });
 
-const Facebook = {
+bot.on('error', (err) => {
+  console.log(err.message);
+});
 
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text;
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err;
+
+    reply({ text }, (err) => {
+      if (err) throw err;
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`);
+    });
+  });
+});
+
+http.createServer(bot.middleware()).listen(3000);
+console.log('Echo bot server running at port 3000.');
+
+const Facebook = {
+	bot: () => {
+    	return bot;
+  	}
 };
 
 module.exports = Facebook;
