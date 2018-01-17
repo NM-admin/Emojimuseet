967b355936cb2060ce281665041235f79bf615e3
Aron Ambrosiani <aron@ambrosiani.se>
2018-01-17 18:39:04 +0100

    hiding facebook

--- script/reply.js
+++ script/reply.js
@@ -4,7 +4,7 @@ const Images = require('../src/images');
 const Twitter = require('../src/twitter');
 const ReplyTweet = require('../src/tweets/reply_tweet');
 const RequestTweet = require('../src/tweets/request_tweet');
-const Facebook = require('../src/facebook');
+//const Facebook = require('../src/facebook');
 
 let stream = Twitter.stream();
 
